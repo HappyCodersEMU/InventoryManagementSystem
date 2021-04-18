b43f442b98c549e2c991743a7506bea41a362f6f
Dovud <dinomov99@gmail.com>
2021-04-18 08:39:12 +0300

    get products endpoint-initial code

---
+++ api/routes/products.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  Product.find()
+    .select("name price _id productImage categoryId description") // Select only listed fields
+    .exec()
+    .then((data) => {
+      const result = {
+        count: data.length,
+        products: data.map((d) => {
+          return {
+            _id: d._id,
+            name: d.name,
+            price: d.price,
+            description: d.description,
+          };
+        }),
+      };
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
