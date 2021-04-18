b3121b295ee275c9d0d417774b72d31f6f37b552
Dovud <dinomov99@gmail.com>
2021-04-18 08:30:13 +0300

    initial server with basic configs and dependencies

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
-const express = require('express')
-const config = require('config')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const morgan = require("morgan"); // Middleware for logs
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const app = express()
+require("dotenv").config();
 
-app.use('/api/auth', require('./routes/auth.routes'))
 
-const PORT = config.get('PORT') || 5000
+try {
 
-async function start() {
-    try {
-        await mongoose.connect(config.get("mongoUri"), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+  mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    },
+    () => { console.log('Mongoose connected') }
+  );
 
-    } catch (e) {
-        console.log("Server Error", e.message)
-        process.exit(1)
+  // Run logging middleware before other routes
+  app.use(morgan("dev"));
+
+  // CORS
+  app.use((req, res, next) => {
+    res.header("Access-Controll-Allow-Origin", "*"); // TODO: Do not allow all domains in production
+    res.header(
+      "Access-Controll-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Controll-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
     }
+    next();
+  });
 
+ 
+
+
+  // Serve static assets if in production
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+
+  // Handle errors.
+  // If the following line is reached, means no routes found
+  app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+
+  module.exports = app;
+
+} catch (e) {
+  console.log("Server Error", e.message)
+  process.exit(1)
 }
-
-start()
