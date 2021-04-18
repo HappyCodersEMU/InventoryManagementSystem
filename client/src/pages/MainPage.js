438bd655f6d687beb59b872a76820220f3d13a62
Egor <ktp332@yandex.ru>
2021-04-18 14:16:31 +0300

    Registration form

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import stylesheet from '../stylesheet.css'
 
 export const MainPage = () => {
     return (
         <div>
-            <h1>Main Page</h1>
+            <div className="background">
+                <header>
+                    <div className="header-content">
+                        <div className="logo-block">
+                            Logo block
+                        </div>
+                        <div className="navbar">
+                            Navbar
+                        </div>
+                        <div className="user-block">
+                            User block
+                        </div>
+                    </div>
+                </header>
+                <div className="container">
+                    <div className="column">
+                    </div>
+                    <div className="main">
+                    </div>
+                    <div className="column">
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -12,37 +12,15 @@ export const useRoutes = isAuthenticated => {
     if (!isAuthenticated) {
         return (
             <Switch>
-                <Route path="/" exact>
-                    <MainPage />
-                </Route>
-                <Route path="/detail/:id">
-                    <DetailPage />
-                </Route>
                 <Route path="/auth/login">
                     <LoginPage />
                 </Route>
                 <Route path="/auth/register">
                     <RegisterPage />
                 </Route>
-                <Redirect to="/" />
+                <Redirect to="/auth/login" />
             </Switch>
         )
     }
-    return (
-        <Switch>
-            <Route path="/" exact>
-                <MainPage />
-            </Route>
-            <Route path="/detail/:id">
-                <DetailPage />
-            </Route>
-            <Route path="/account">
-                <AccountPage />
-            </Route>
-            <Route path="/organization">
-                <OrganizationPage />
-            </Route>
-            <Redirect to="/" />
-        </Switch>
-    )
+
 }
