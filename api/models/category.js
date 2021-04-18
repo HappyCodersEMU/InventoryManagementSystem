e3d4e589c637adc99c2d7beb4119579ceb4f1bb4
Dovud <dinomov99@gmail.com>
2021-04-18 08:36:09 +0300

    initial product schema-will be modified

---
+++ api/models/category.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  description: { type: String, default: "" },
+  categoryImage: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
