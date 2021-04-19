de7b7806f0deb65fd90b5a7b0647543696355e34
Dovud <dinomov99@gmail.com>
2021-04-19 19:07:25 +0300

    subscription models created

---
+++ api/models/subscription.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const subscriptionSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    description: { type: String },
+    transPerMonth: { type: Number, required: true },
+    numProducts: { type: Number, required: true },
+    numMembers: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Subscription", subscriptionSchema);
