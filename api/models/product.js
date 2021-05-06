b0a42953422c8f0d1ce45154429cd3b71e78f259
Dovud <dinomov99@gmail.com>
2021-05-06 19:41:40 +0300

    updated product fields

--- api/models/product.js
+++ api/models/product.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  productCode: { type: String },
   name: { type: String, required: true },
   price: { type: Number, required: false },
   productImage: { type: String, required: false },
@@ -9,6 +10,11 @@ const productSchema = mongoose.Schema({
   categoryId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
+    required: true,
+  },
+  subcategoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subcategory",
     required: true,
   },
   details: { type: Object, default: {} },
