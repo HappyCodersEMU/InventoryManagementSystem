9135458694ae1b08aede18d3551346d1c9a48eab
Egor <ktp332@yandex.ru>
2021-05-15 15:01:23 +0300

    Partial realisation of Modal window for TableBuy

--- client/src/pages/components/ApplicationComponents/BuyList.js
+++ client/src/pages/components/ApplicationComponents/BuyList.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const BuyList = () => {
 
+    const test = () => {
+        const search = window.location.search.replace('?', '')
+    }
+
     return (
         <>
             BuyList
+            <button onClick={test}>
+                test
+            </button>
         </>
     );
 }
