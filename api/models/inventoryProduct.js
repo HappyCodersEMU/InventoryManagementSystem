e763655c9b8566fc21ca81238b2a5a4eb7da1b8e
Dovud <dinomov99@gmail.com>
2021-05-18 22:28:04 +0300

    init

--- api/models/inventoryProduct.js
+++ api/models/inventoryProduct.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const inventoryProductSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  productId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product"},
-  companyId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Company" },
-  companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Storage" },
-  quantity: { type: Number, required: true },
+  product: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product" },
+  company: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Company" },
+  quantity: { type: Number },
+  // storageId: { type: mongoose.Schema.Types.ObjectId, ref: "Storage" },
 });
 
 module.exports = mongoose.model("InventoryProduct", inventoryProductSchema);
