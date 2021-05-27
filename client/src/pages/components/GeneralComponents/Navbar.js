6793b918a7803bba6491d550bbd4e8f54897413b
ktp33 <ktp332@yandex.ru>
2021-05-27 16:35:59 +0300

    Next features are implemented:    Buttons in Navbar appears depends on role of the user    TableMembers.js updates after new member is added

--- client/src/pages/components/GeneralComponents/Navbar.js
+++ client/src/pages/components/GeneralComponents/Navbar.js
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 import './Navbar.css'
 
 
-function Navbar({ companyId }) {
+function Navbar({ companyId, validateRole, userRoles }) {
 
     return (
         <>
+            { validateRole([1, 2, 3],userRoles) &&
             <div className="navbar-link">
-                <NavLink to={`/${companyId}/products`} >
+                    <NavLink to={`/${companyId}/products`} >
                     <div className="navbar-link-content-wrap">
                         <div className="navbar-link-content">
                             <div className="navbar-link-content-icon-wrap" >
@@ -24,12 +25,16 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
+
+            { validateRole([1, 2, 3],userRoles) &&
             <div className="navbar-link">
                 <NavLink to={`/${companyId}/inventory`} >
                     <div className="navbar-link-content-wrap">
                         <div className="navbar-link-content">
-                            <div className="navbar-link-content-icon-wrap">
-                                <div className="navbar-link-content-icon">
+                            <div className="navbar-link-content-icon-wrap" >
+                                <div className="navbar-link-content-icon" >
+
                                 </div>
                             </div>
                             <div className="navbar-link-content-text">
@@ -40,6 +45,8 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
+            { validateRole([1, 3, 4],userRoles) &&
             <div className="navbar-link">
                 <NavLink to={`/${companyId}/billing`} >
                     <div className="navbar-link-content-wrap">
@@ -57,12 +64,16 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
+
+            { validateRole([1, 3],userRoles) &&
             <div className="navbar-link">
                 <NavLink to={`/${companyId}/buy`} >
                     <div className="navbar-link-content-wrap">
                         <div className="navbar-link-content">
-                            <div className="navbar-link-content-icon-wrap">
-                                <div className="navbar-link-content-icon">
+                            <div className="navbar-link-content-icon-wrap" >
+                                <div className="navbar-link-content-icon" >
+
                                 </div>
                             </div>
                             <div className="navbar-link-content-text">
@@ -73,12 +84,16 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
+
+            { validateRole([1, 3],userRoles) &&
             <div className="navbar-link">
                 <NavLink to={`/${companyId}/sell`} >
                     <div className="navbar-link-content-wrap">
                         <div className="navbar-link-content">
-                            <div className="navbar-link-content-icon-wrap">
+                            <div className="navbar-link-content-icon-wrap" >
                                 <div className="navbar-link-content-icon" >
+
                                 </div>
                             </div>
                             <div className="navbar-link-content-text">
@@ -89,8 +104,11 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
+
+            { validateRole([1, 2],userRoles) &&
             <div className="navbar-link">
-                <NavLink to={`/${companyId}/more`} >
+                <NavLink to={`/${companyId}/more`}>
                     <div className="navbar-link-content-wrap navbar-link-content-wrap-more">
                         <div className="navbar-link-content">
                             <div className="navbar-link-content-icon-wrap">
@@ -105,8 +123,9 @@ function Navbar({ companyId }) {
                 </NavLink>
                 <hr className="input-line"/>
             </div>
+            }
         </>
     );
 }
 
-export default Navbar;
+export default Navbar
