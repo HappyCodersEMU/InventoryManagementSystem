c2c34ad1e54290ae8d4fadf36bd17761f1b24bd7
Dovud <dinomov99@gmail.com>
2021-06-06 00:21:04 +0300

    transactions page (FE)

--- client/src/pages/components/ApplicationComponents/SellList.js
+++ client/src/pages/components/ApplicationComponents/SellList.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHttp } from "./../../../hooks/http.hook";
+import { Loader } from "./../GeneralComponents/Loader";
+import './ProductComponents/TableProducts.css'
 
-const SellList = () => {
+function SellList({ companyId }) {
 
+    const { request } = useHttp()
+
+    // Initial data
+    const [transactions, setTransaction] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(async () => {
+        const data = await request(`/api/transactions?companyId=${companyId}`, 'GET')
+        setIsLoading(false)
+        setTransaction(data)
+
+    }, [])
+
+
+    if (isLoading) {
+        return <Loader />
+    }
+    if (transactions.length < 1) {
+        return (
+            "No records"
+        )
+    }
     return (
         <>
-            Not implemented
+            <div className="table-wrap">
+                <table className="table table-products">
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Date</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {transactions.map((item) => (
+                            <tr key={item.productCode}>
+                                <td>{item.productCode}</td>
+                                <td>{item.productName}</td>
+                                <td>{item.price}</td>
+                                <td>{new Date(item.date).toLocaleString()}</td>
+                                <td>{item.quantity}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <hr />
         </>
     );
 }
